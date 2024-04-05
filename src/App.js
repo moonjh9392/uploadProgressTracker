deaeb9836ed6510fae4a244a3411903987454cc8
butter-moonjh <151711779+butter-moonjh@users.noreply.github.com>
2024-04-05 13:12:29 +0900

    초기셋팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+
+const AppWrap = styled.div`
+  padding: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10%;
+`;
+
+const ListWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
+  //1. 파일 추가 누르면 파일선택 팝업
+  //2. 변환 선택시 api로 파일전송 후 res값으로 해당 파일 key값받음
+  //3. ws으로 파일 변환 상태값 갱신
+  const [upLoadList, setUpLoadList] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap>
+      <button>파일추가</button>
+      <ListWrap></ListWrap>
+    </AppWrap>
   );
 }
 
